d913aee03e691fc3d39f5ba97b69176e572187fa
Katariina Pohto <63994411+atarnus@users.noreply.github.com>
2023-04-13 02:41:15 +0300

    UI work: list all + link to single

--- UI/script.js
+++ UI/script.js
@@ -1,15 +1,12 @@
 // Function to empty all fields and backgrounds
 function clean() {
     document.getElementById("headline").innerHTML = "";
-    document.getElementById("quote-box").innerHTML = "";
     document.getElementById("quote-list").innerHTML = "";
+    document.getElementById("quote-text").innerHTML = "";
+    document.getElementById("quote-work").innerHTML = "";
+    document.getElementById("quote-chars").innerHTML = "";
     // Remove background from quote box
     document.getElementById("quote-box").classList.remove("card");
-}
-
-// Function to add the card box to div
-function cardBox() {
-    document.getElementById("quote-box").classList.add("card");
 }
 
 // Function to fetch data from API and transform it to JS
@@ -19,6 +16,8 @@ async function getData(url) {
     const parsed = JSON.parse(text);
     return parsed;
 }
+
+// TEXT MODIFICATIONS
 
 // Function to clip the quote to a certain size
 function truncate(str, n) {
@@ -39,6 +38,14 @@ function quoteToCard(str) {
     return '</p><p class="card-text">' + str.split(search).join(replaceWith) + '</p>';
 }
 
+
+// QUOTE BOX
+
+// Function to add the card box to div
+function cardBox() {
+    document.getElementById("quote-box").classList.add("card");
+}
+
 // Function to list characters from array
 function characters(array) {
     let str = "";
@@ -49,11 +56,22 @@ function characters(array) {
     return '<p class="card-subtitle">' + str + '</p>';
 }
 
-// Function to get a random quote
-async function randomQuote() {
-    let item = await getData("https://geronimo.okol.org/~pohkat/quote-api/random.php");
-    cardBox();
+// Get a random quote
+function randomQuote() {
+    singleQuote("https://geronimo.okol.org/~pohkat/quote-api/random");
+}
 
+// Get a quote by id
+function quoteById(id) {
+    let url = "https://geronimo.okol.org/~pohkat/quote-api/quote/" + id;
+    singleQuote(url);
+}
+
+// Function to get a single quote
+async function singleQuote(url) {
+    clean();
+    let item = await getData(url);
+    cardBox();
     let quote = quoteToCard(item.quote);
     let work = '<p class="card-subtitle">' + item.work + ' by ' + item.author + '</p>';
     let series = "";
@@ -64,10 +82,35 @@ async function randomQuote() {
         series = '<p class="card-subtitle">' + item.series + '</p>';
     }
 
+    console.log(quote);
     document.getElementById("quote-text").innerHTML = quote;
-    document.getElementById("work").innerHTML = work + series;
-    document.getElementById("chars").innerHTML = chars;
+    document.getElementById("quote-work").innerHTML = work + series;
+    document.getElementById("quote-chars").innerHTML = chars;
 }
 
-// For the listing
-// let quote = lineToSpace(truncate(item.quote, 75));
+// LISTINGS
+
+const TABLE = "<table><tr><th>ID</th><th>Quote</th><th>Author</th><th>Work</th><th>Series</th></tr>";
+
+async function listAllQuotes() {
+    clean();
+    let item = await getData("https://geronimo.okol.org/~pohkat/quote-api/quote");
+    let array = item.results;
+    let list = "";
+    let series;
+
+    for (const x in array) {
+        let obj = array[x];
+
+        // Clear null if no series exist
+        if (obj.series) {
+            series = obj.series;
+        } else {
+            series = "";
+        }
+
+        list += "<tr class=\"link\" onclick=\"quoteById(" + obj.id + ")\"><td>" + obj.id + "</td><td>" + lineToSpace(truncate(obj.quote, 75)) + "</td><td>" + obj.author + "</td><td>" + obj.work + "</td><td>" + series + "</td></tr>";
+    }
+
+    document.getElementById("quote-list").innerHTML = TABLE + list + "</table>";
+}
